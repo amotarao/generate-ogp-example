56ae8c4edeaeef01a38327c798b2a8aedfd1ff36
Amon Sawamura <amon.sawamura@gmail.com>
2019-03-21 23:49:08 +0900

    feat: example

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-import { Request, Response } from 'express';
+/* global document */
 
-export const example = async (req: Request, res: Response): Promise<void> => {};
+import { Request, Response } from 'express';
+import * as puppeteer from 'puppeteer';
+
+export const example = async (req: Request, res: Response): Promise<void> => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1280, height: 720 });
+
+  await page.evaluate(() => {
+    const text = document.createElement('div');
+    text.style.width = '100vw';
+    text.style.height = '100vh';
+    text.style.fontSize = '40px';
+    text.innerText = 'Example Text';
+    document.body.appendChild(text);
+  });
+
+  await page.screenshot({ path: 'example.png' });
+
+  res.status(200).sendFile('example.png');
+};
