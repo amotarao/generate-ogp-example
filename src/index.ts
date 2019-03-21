43bccbf5617f597a0219c9f07b51021f24ae635a
Amon Sawamura <amon.sawamura@gmail.com>
2019-03-22 01:04:46 +0900

    fix html

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 /* global document */
 
-import * as fs from 'fs';
 import { Request, Response } from 'express';
 import * as puppeteer from 'puppeteer';
-
-const baseHtml = fs.readFileSync('./base.html', 'utf8');
 
 export const example = async (req: Request, res: Response): Promise<void> => {
   const text = req.query['text'] || 'Example Text';
@@ -14,7 +11,9 @@ export const example = async (req: Request, res: Response): Promise<void> => {
   });
   const page = await browser.newPage();
   await page.setViewport({ width: 1200, height: 630 });
-  await page.setContent(baseHtml);
+  await page.setContent(
+    '<!DOCTYPE html><html lang="ja"><head><meta charset="UTF-8" /><meta name="viewport" content="width=device-width, initial-scale=1.0" /><meta http-equiv="X-UA-Compatible" content="ie=edge" /><title>Document</title><link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c" rel="stylesheet"/><style>* {margin: 0;padding: 0;}#app {align-items: center;display: flex;justify-content: center;}#text {font-family: "M PLUS Rounded 1c";font-size: 40px;}</style></head><body><div id="app"><p id="text"></p></div></body></html>'
+  );
 
   await page.evaluate(() => {
     const textElm = document.getElementById('text') as HTMLParagraphElement;
