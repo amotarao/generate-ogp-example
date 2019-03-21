d2cb4b2e604c83af566822c51ff469ebe68e0e2a
Amon Sawamura <amon.sawamura@gmail.com>
2019-03-22 00:48:13 +0900

    refactor(html): create base html

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 /* global document */
 
+import * as fs from 'fs';
 import { Request, Response } from 'express';
 import * as puppeteer from 'puppeteer';
+
+const baseHtml = fs.readFileSync('./base.html', 'utf8');
 
 export const example = async (req: Request, res: Response): Promise<void> => {
   const browser = await puppeteer.launch({
@@ -9,14 +12,11 @@ export const example = async (req: Request, res: Response): Promise<void> => {
   });
   const page = await browser.newPage();
   await page.setViewport({ width: 1200, height: 630 });
+  await page.setContent(baseHtml);
 
   await page.evaluate(() => {
-    const text = document.createElement('div');
-    text.style.width = '100vw';
-    text.style.height = '100vh';
-    text.style.fontSize = '40px';
+    const text = document.getElementById('text') as HTMLParagraphElement;
     text.innerText = 'Example Text';
-    document.body.appendChild(text);
   });
 
   const png = await page.screenshot({
