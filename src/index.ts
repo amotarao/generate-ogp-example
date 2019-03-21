93c103154e4ccbfe9fb6378bd9eb1a9de1063ae9
Amon Sawamura <amon.sawamura@gmail.com>
2019-03-22 01:26:12 +0900

    idle

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import * as puppeteer from 'puppeteer';
 
 export const example = async (req: Request, res: Response): Promise<void> => {
   const text: string = req.query['text'] || 'Example Text';
+  console.log(`text: ${text}`);
 
   const browser = await puppeteer.launch({
     args: ['--no-sandbox'],
@@ -12,7 +13,10 @@ export const example = async (req: Request, res: Response): Promise<void> => {
   const page = await browser.newPage();
   await page.setViewport({ width: 1200, height: 630 });
   await page.setContent(
-    '<!DOCTYPE html><html lang="ja"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge"><link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c" rel="stylesheet"/><style>*{margin:0;padding:0;}#app{align-items:center;display:flex;justify-content:center;}#text{font-family:"M PLUS Rounded 1c";font-size: 40px;}</style></head><body><div id="app"><p id="text"></p></div></body></html>'
+    '<!DOCTYPE html><html lang="ja"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge"><link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c" rel="stylesheet"/><style>*{margin:0;padding:0;}#app{align-items:center;display:flex;justify-content:center;}#text{font-family:"M PLUS Rounded 1c";font-size: 40px;}</style></head><body><div id="app"><p id="text"></p></div></body></html>',
+    {
+      waitUntil: 'networkidle0',
+    }
   );
 
   await page.evaluate((text: string) => {
